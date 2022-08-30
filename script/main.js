632c78c9b03d2d5df53535614cd922ad1d0516ad
Valeri Zanguri <zanguraa@gmail.com>
2022-08-30 18:38:45 +0400

    import API from beck-end with fetch metod

---
+++ script/main.js
@@ -0,0 +1,38 @@
+const catchTeam = document.getElementById('team');
+
+console.log(position);
+
+fetch('https://pcfy.redberryinternship.ge/api/teams')
+    .then(response => response.json()).then(answer => {
+        let teams = answer.data;
+        
+        for(let i = 0; i < teams.length; i++) {
+            let option = document.createElement('option');
+            option.value = teams[i].id;
+            option.innerText = teams[i].name;
+            team.appendChild(option);
+        }
+    }
+)
+
+catchTeam.addEventListener('change', () => {
+    position.disabled = false;
+   
+    position.innerHTML = '<option value="position" disabled selected>პოზიცია</option>';
+    const teamValue = catchTeam.value;
+
+    fetch('https://pcfy.redberryinternship.ge/api/positions') 
+    .then(response => response.json()).then( answer => {
+        let positions = answer.data;
+        for(let i = 0; i < positions.length; i++) {
+            let option = document.createElement('option');
+            option.value = positions[i].id;
+            option.innerText = positions[i].name;
+            if(positions[i].team_id == teamValue) {
+                position.appendChild(option);
+            }
+        }
+    }
+)
+})
+
